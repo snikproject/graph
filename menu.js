ddad3ee711f8390af48e24157c62cd0032e2cb5f
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2016-08-15 15:51:45 +0200

    adding missing menu script and css file.

---
+++ menu.js
@@ -0,0 +1,21 @@
+function help() {
+    document.getElementById("help").classList.toggle("show");
+}
+
+function about() {
+    window.alert("SNIK Graph version "+MODIFIED+"\nOntology version "+ONTOLOGY_MODIFIED);
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var d = 0; d < dropdowns.length; d++) {
+      var openDropdown = dropdowns[d];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
