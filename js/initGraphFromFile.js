c691fbeb50352200d77946524c7aef5e238ba4e2
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2017-07-05 15:35:23 +0200

    Remove old commented out code from js/initGraphFromFile.js and fix finally -> catch.

--- js/initGraphFromFile.js
+++ js/initGraphFromFile.js
@@ -12,42 +12,19 @@ function initGraphFromFile()
     cache: 'default',
     credentials: 'include'};
 
-  /*
-  // only needed if there is an access protected file with the full information that is sometimes present.
-  fetch("data/snik-ciox.cyjs",init)
-  .then(response => {return response.json();})w
-  .then(json =>
-  {
-  initGraph(document.getElementById('cy'),json);
-  cy.zoom(0.1);
-  cy.center(cy.elements().nodes().filter(`node[name= "http://www.snik.eu/ontology/meta/EntityType"]`)[0]);
-  progress(100);
-})
-.catch(err =>
-{
-console.log('snik-ciox.cyjs not found, loading snik.cyjs. Details: '+err);
-*/
-
   return fetch("data/snik.cyjs",init)
-    .then(response=>response.json())
-    .then(json=>
+  .then(response=>response.json())
+  .then(json=>
     {
-      graph.initGraph();
-      graph.cy.add(json.elements);
-    //cy.zoom(0.1);
-    //cy.center(cy.elements().nodes().filter(`node[name= "http://www.snik.eu/ontology/meta/EntityType"]`)[0]);
-      return graph.cy;
+    graph.initGraph();
+    graph.cy.add(json.elements);
+    return graph.cy;
+  })
+    .catch(e=>
+      {
+      alert('Error loading snik.cyjs: '+e);
     })
-    .catch(err2=>
-    {
-      alert('Did not find graph file snik.cyjs, failed to load graph. Details: '+err2);
-      progress(100);
-    })
-    .finally(()=>
-    {
-      progress(100);
-    });
-  //});
+      .then(()=> {progress(100);});
 }
 
 export {initGraphFromFile};
