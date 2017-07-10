38e5778dca58e9303c864cc3f0abe9192a5fb40d
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2017-07-10 15:15:04 +0200

    change order of prefixes to fix error when prefixes are prefixes of other prefixes.

--- js/rdf.js
+++ js/rdf.js
@@ -1,14 +1,15 @@
-const prefixes =
+const prefixes = // order important! substrings of other prefixes must come later
 [
   ["meta","http://www.snik.eu/ontology/meta/"],
   ["bb","http://www.snik.eu/ontology/bb/"],
   ["ob","http://www.snik.eu/ontology/ob/"],
   ["he","http://www.snik.eu/ontology/he/"],
   ["ciox","http://www.snik.eu/ontology/ciox/"],
-  ["it","http://www.snik.eu/ontology/it/"],
   ["it4it","http://www.snik.eu/ontology/it4it/"],
+  ["it","http://www.snik.eu/ontology/it/"],
   ["skos","http://www.w3.org/2004/02/skos/core#"],
   ["rdfs","http://www.w3.org/2000/01/rdf-schema#"],
+  ["rdf","http://www.w3.org/1999/02/22-rdf-syntax-ns#"],
 ];
 
 export function short(uri)
