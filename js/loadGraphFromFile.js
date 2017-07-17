be68a8f5e164fdf908d416fab6f78ba565681b2a
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2017-07-17 15:06:32 +0200

    add file timers

--- js/loadGraphFromFile.js
+++ js/loadGraphFromFile.js
@@ -1,5 +1,6 @@
 import {progress} from "./progress.js";
 import * as graph from "./graph.js";
+import timer from "./timer.js";
 
 export default function loadGraphFromFile()
 {
@@ -11,12 +12,15 @@ export default function loadGraphFromFile()
     mode: 'cors',
     cache: 'default',
     credentials: 'include'};
-
+  const loadTimer = timer("file-load");
   return fetch("data/snik.cyjs",init)
     .then(response=>response.json())
     .then(json=>
     {
+      loadTimer.stop();
+      const addTimer = timer("file-add");
       graph.cy.add(json.elements);
+      addTimer.stop();
     })
     .catch(e=>
     {
