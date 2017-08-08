a538eac0e7ee7311cd4b484a416c31db0f93ece9
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2017-08-08 12:15:52 +0200

    add json download button and functionality.

--- js/button.js
+++ js/button.js
@@ -2,11 +2,29 @@ import * as graph from "./graph.js";
 //import * as history from "./history.js";
 import loadGraphFromSparql from "./loadGraphFromSparql.js";
 
+var saveData = (function ()
+{
+  var a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  return function (data, fileName)
+  {
+    var json = JSON.stringify(data),
+      blob = new Blob([json], {type: "octet/stream"}),
+      url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = fileName;
+    a.click();
+    window.URL.revokeObjectURL(url);
+  };
+}());
+
 const buttonConfigs =
 [
   ["Reset View", graph.resetStyle],
   //  ["History",   history.showHistory],
   ["Reload", loadGraphFromSparql],
+  ["Export", ()=>{saveData(graph.cy.json(),"snik.json");}],
 ];
 export default function addButtons()
 {
