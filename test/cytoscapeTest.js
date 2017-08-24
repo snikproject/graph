bcf097362e0207e60cf0ade695e91bfaa56a81ff
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2017-08-24 10:48:55 +0200

    create function placeholders for layout loading in cytoscape test.

--- test/cytoscapeTest.js
+++ test/cytoscapeTest.js
@@ -19,18 +19,35 @@ describe('cytoscape', function()
     assert(cy);
     assert(cy._private.layout===null);
   });
-  it('loadGraphFromSparql', function()
+  it('load graph from sparql', function()
   {
     return loadGraphFromSparql(cy, subs).then(()=>
       assert.closeTo(cy.nodes().size(),100,50)
     );
   });
-  it('apply euler layout', function()
+  it('calculate layout', function()
   {
     // Causes "TypeError: Cannot read property 'pos' of undefined"
     // see https://github.com/cytoscape/cytoscape.js-euler/issues/14
     //layout.run(cy,layout.euler,subs);
     // use cose for now
-    assert(layout.run(cy,layout.cose,subs));
+    //assert(layout.run(cy,layout.cose,subs));
+    // cose places all nodes on 0|0, use grid for now
+    assert(layout.run(cy,layout.grid,subs));
+
+    const nodes = cy.nodes();
+    for(let i=0;i<nodes.size();i++)
+    {
+      for(let j=i+1;j<nodes.size();j+=10)
+      {
+        assert(JSON.stringify(nodes[i].position())!==JSON.stringify(nodes[j].position()),()=>"2 nodes at the same position "+ JSON.stringify(nodes[i].position()));
+      }
+    }
+  });
+  it('load layout from file', function()
+  {
+  });
+  it('load layout from cache', function()
+  {
   });
 });
