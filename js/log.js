a05bb1936be9a9620833cd305b18199dcda8eaa6
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2018-10-22 20:13:52 +0200

    Reformatted log.js

--- js/log.js
+++ js/log.js
@@ -4,75 +4,91 @@ Logging to console or overlay or both, depending on availability (server call us
 /* eslint no-console: 0 */
 // temporary solution. todo: use a logging library or write more elegantly using enums or integers
 
-
 /**
  * callSource - description
  *
  * @return {type}  description
 
  */
-function callSource()
-{
+function callSource() {
   // https://stackoverflow.com/questions/1340872/how-to-get-javascript-caller-function-line-number-how-to-get-javascript-caller
   var e = new Error();
-  if(!e.stack) {return "";} // old browser
+  if (!e.stack) {
+    return '';
+  } // old browser
   var stack = e.stack.toString().split(/\r\n|\n/);
-  return "            [" + stack[2].replace(/.*\//,"") + ']';
+  return '            [' + stack[2].replace(/.*\//, '') + ']';
 }
 
 /** Logs the given messages with log level info */
-export function info(s)
-{
-  if(typeof config !== 'undefined'&&config.logLevelDisplay==="info"||config.logLevelDisplay==="debug"||config.logLevelDisplay==="trace") {alert(s);}
-  if(!config||config.logLevelConsole==="info"||config.logLevelConsole==="debug"||config.logLevelConsole==="trace")
-  {
+export function info(s) {
+  if (
+    (typeof config !== 'undefined' && config.logLevelDisplay === 'info') ||
+    config.logLevelDisplay === 'debug' ||
+    config.logLevelDisplay === 'trace'
+  ) {
+    alert(s);
+  }
+  if (
+    !config ||
+    config.logLevelConsole === 'info' ||
+    config.logLevelConsole === 'debug' ||
+    config.logLevelConsole === 'trace'
+  ) {
     const argumentsArray = Array.prototype.slice.call(arguments);
     argumentsArray.push(callSource());
-    console.log.apply(console,argumentsArray);
+    console.log.apply(console, argumentsArray);
   }
 }
 
 /** Logs the given messages with log level info */
-export function debug(s)
-{
-  if(typeof config !== 'undefined'&&config.logLevelDisplay==="debug"||config.logLevelDisplay==="trace")  {alert(s);}
-  if(typeof config === 'undefined'||config.logLevelConsole==="debug"||config.logLevelConsole==="trace")
-  {
+export function debug(s) {
+  if (
+    (typeof config !== 'undefined' && config.logLevelDisplay === 'debug') ||
+    config.logLevelDisplay === 'trace'
+  ) {
+    alert(s);
+  }
+  if (
+    typeof config === 'undefined' ||
+    config.logLevelConsole === 'debug' ||
+    config.logLevelConsole === 'trace'
+  ) {
     const argumentsArray = Array.prototype.slice.call(arguments);
     argumentsArray.push(callSource());
-    console.log.apply(console,argumentsArray);
+    console.log.apply(console, argumentsArray);
   }
 }
 
 /** Logs the given messages with log level error
  Errors are always shown both on console and display.*/
-export function error(s)
-{
+export function error(s) {
   alert(s);
   {
     const argumentsArray = Array.prototype.slice.call(arguments);
     argumentsArray.push(callSource());
-    console.log.apply(console,argumentsArray);
+    console.log.apply(console, argumentsArray);
   }
 }
 
 /** Logs the given messages with log level warn */
-export function warn(s)
-{
-  if(typeof config !== 'undefined'&&config.logLevelDisplay!=="error")  {alert(s);}
-  if(typeof config === 'undefined'||config.logLevelConsole!=="error")
-  {
+export function warn(s) {
+  if (typeof config !== 'undefined' && config.logLevelDisplay !== 'error') {
+    alert(s);
+  }
+  if (typeof config === 'undefined' || config.logLevelConsole !== 'error') {
     const argumentsArray = Array.prototype.slice.call(arguments);
     argumentsArray.push(callSource());
-    console.log.apply(console,argumentsArray);
+    console.log.apply(console, argumentsArray);
   }
 }
 
 /** Logs the given messages with log level trace. Never displayed to the user. */
-export function trace()
-{
-  if(typeof config === 'undefined'||config.logLevelConsole!=="trace") {return;}
+export function trace() {
+  if (typeof config === 'undefined' || config.logLevelConsole !== 'trace') {
+    return;
+  }
   const argumentsArray = Array.prototype.slice.call(arguments);
   argumentsArray.push(callSource());
-  console.log.apply(console,argumentsArray);
+  console.log.apply(console, argumentsArray);
 }
