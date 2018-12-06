ea121119e36398855f871fec12fb3776fe1e0337
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2018-12-06 17:46:58 +0100

    Add prototype of a language specific string file for English.

---
+++ js/lang/en.js
@@ -0,0 +1,41 @@
+export default
+{
+  lang: "en",
+  strings:
+  {
+    // menu *******************************************************************
+    // file **********************************************
+    "file": "File",
+    "loadGraphWithLayout": "Load Graph File with Layout",
+    "loadLayout": "Load Layout",
+    "loadSparql": "Load from SPARQL endpoint",
+    "saveCytoscapeFull": "Save Full Graph with Layout as Cytoscape File",
+    "saveCytoscapeVisible": "Save Visible Graph with Layout as Cytoscape File",
+    "saveLayout": "Save Layout only",
+    "recalculateLayoutAndReplace":"Recalculate Layout and Replace in Browser Cache",
+    "saveImageCurrentView": "Save Image of Current View",
+    "saveImageWholeGraph": "Save Image of Whole Graph",
+    "saveImageCurrentViewHighRes": "Save Image of Current View (high res)",
+    "saveImageWholeGraphHighRes": "Save Image of Whole Graph (high res)",
+    // filter ********************************************
+    "filter": "Filter",
+    // options********************************************
+    "options": "Options",
+    "separateSubs": "Separate Subontologies",
+    "cumulativeSearch": "Cumulative Search",
+    "dayMode": "Day Mode",
+    // services ******************************************
+    "services": "Services",
+    "sparqlEndpoint": "SPARQL Endpoint",
+    "rdfBrowser": "RDF Browser",
+    "dataQualityEvaluation": "Data Quality Evaluation",
+    // language ******************************************
+    "language": "Language",
+    "english": "English",
+    "german": "German",
+    "persian": "Persian",
+    // help **********************************************
+    //"help": "Help",
+    // don't translate the help options yet because they are English anyways
+  },
+};
