79a472113d6da6b3da9000c1c8a5f403a4994952
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2019-01-31 16:06:09 +0100

    Start implementing a test for the future fuse search feature.

--- test/searchTest.js
+++ test/searchTest.js
@@ -11,7 +11,7 @@ function equals(as, bs)
 
 describe('search', function()
 {
-  it('#search()', function()
+  it('#exactSearch()', function()
   {
     const queries = ["Chief Information Officer","chief information officer","chiefinformationofficer"];
     const promises = queries.map(q=>search.search(q));
@@ -30,5 +30,41 @@ Result 2: "${[...results[j]]}"`);
         }
       }
     });
+  });
+  it('#fuzzySearch()', function()
+  {
+    // each of the terms in the first array of each entry should lead to each in the second array
+    const benchmark = [
+      // space and case insensitive
+      [["Chief Information Officer","chief information officer","chiefinformationofficer","CiO"],["bb:ChiefInformationOfficer","ob:ChiefInformationOfficer","he:ChiefInformationOfficer","ciox:ChiefInformationOfficer"]],
+      // ² / 2
+      [["3LGM2 Service Class","3LGM²-S Service Class"],["bb:3LGM2SServiceClass"]],
+      [["DurchfuehrungJourFixeCEO","Durchfuehrung Jour-Fixe CEO","Durchführung Jour-Fixé CEO"],["ciox:DurchfuehrungJourFixeCEO"]],
+    ];
+    for(const entry of benchmark)
+    {
+      const queries = entry[0];
+      const resources = entry[1];
+      /*
+      for(const query of queries)
+      {
+        const promises = queries.map(q=>search.search(q));
+        return Promise.all(promises).then(results=>
+        {
+          // pairwise equality check
+          for(let i=0;i<results.length;i++)
+          {
+            assert.isAbove(results[i].size,3,"not enough hits for "+queries[i]+": "+[...results[i]].join(' '));
+            for(let j=i+1;j<results.length;j++)
+            {
+              assert(equals(results[i],results[j]),
+                `different search results for queries "${queries[i]}" and "${queries[j]}":
+    Result 1: "${[...results[i]]}"
+    Result 2: "${[...results[j]]}"`);
+            }
+          }
+        });
+*/
+    }
   });
 });
