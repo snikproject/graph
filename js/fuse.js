179b40f483b2535f1d1965d701536f8cf25093c8
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2019-02-06 16:46:47 +0100

    Tweak fuse search.

--- js/fuse.js
+++ js/fuse.js
@@ -10,26 +10,30 @@ const options =
 {
   shouldSort: true,
   tokenize: true,
+  threshold: 0.25,
   maxPatternLength: 40,
+  minMatchCharLength: 3,
+  matchAllTokens: true,
+  location: 0,
+  distance: 100,
   id: "uri",
   keys:
   [
-    {name:"l", weight: 0.3},
-    {name:"al", weight: 0.3},
-    {name:"def", weight: 0.1},
-    {name:"suffix", weight: 0.3},
+    {name:"l", weight: 0.7},
+    {name:"def", weight: 0.3},
   ],
 };
 
 /** Create fulltext index from SPARQL endpoint. */
 export async function createIndex()
 {
+  log.info("Create Fuse Search Index");
   const froms = config.allSubOntologies.map(sub=>`from <http://www.snik.eu/ontology/${sub}>`).reduce((a,b)=>a+"\n"+b);
   const sparqlQuery = `select
-  replace(replace(str(?c),"http://www.snik.eu/ontology/",""),"/",":") as ?uri
-  group_concat(distinct(str(?l));separator=" ") as ?l
-  group_concat(distinct(str(?al));separator=" ") as ?al
-  group_concat(distinct(str(?def));separator=" ") as ?def
+  ?c as ?uri
+  group_concat(distinct(str(?l));separator="|") as ?l
+  group_concat(distinct(str(?al));separator="|") as ?al
+  group_concat(distinct(str(?def));separator="|") as ?def
   ${froms}
   {
     ?c a owl:Class.
@@ -39,17 +43,16 @@ export async function createIndex()
   }`;
   const bindings = await sparql.select(sparqlQuery);
   const items = [];
-
+  const i=0;
   for(const b of bindings)
   {
     const item = {};
     items.push(item);
-    for(const key of Object.keys(b))
-    {
-      const v = b[key].value;
-      if(v) {item[key]=v;}
-    }
-    item.suffix = item.uri.replace(".*/","");
+    const suffix = b.uri.value.replace(/.*\//,"");
+    item.uri = b.uri.value;
+    item.l = [...b.l.value.split('|'),suffix];
+    if(b.al.value) {item.l = [...item.l,...b.al.value.split('|')];}
+    if(b.def.value) {item.def = b.def.value;}
   }
   index = new Fuse(items,options);
   return items; // for testing
@@ -62,6 +65,5 @@ export async function search(userQuery)
 {
   if(!index) {await createIndex();}
   const result = index.search(userQuery);
-  log.debug(result);
   return result;
 }
