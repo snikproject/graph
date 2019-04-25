277c62a6271a2a8d40b765ef17bbc2caecaea2d3
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2019-04-25 10:59:42 +0200

    Fix link for "Submit Feedback about the Ontology".

--- js/browser/menu.js
+++ js/browser/menu.js
@@ -206,7 +206,7 @@ function menuData()
         ["contribute.html","Contribute"],
         ["http://www.snik.eu/","Project Homepage"],
         [about,"About SNIK Graph"],
-        ["https://github.com/IMISE/snik-ontowww.snik.eu/ontology/issues","Submit Feedback about the Ontology"],
+        ["https://github.com/IMISE/snik-ontology/issues","Submit Feedback about the Ontology"],
         [visualizationFeedback,"Submit Feedback about the Visualization"],
       ],
     },
