efb5379a3d5fb87381e58b806785e9accfb6f9cb
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2019-06-06 13:40:45 +0200

    Add Webpack circular dependency plugin.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import WebpackAutoInject from 'webpack-auto-inject-version';
+import  CircularDependencyPlugin from 'circular-dependency-plugin';
 
 export default {
   entry: path.join(__dirname, 'js/browser/main.js'),
@@ -21,5 +22,5 @@ export default {
   },
   mode: 'development',
   devtool: 'source-map',
-  plugins: [new WebpackAutoInject()],
+  plugins: [new WebpackAutoInject(),  new CircularDependencyPlugin({exclude: /a\.js|node_modules/,failOnError: true,cwd: process.cwd()})],
 };
