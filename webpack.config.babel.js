a773b240606583cb245bb7c949332b983dc719b5
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2020-01-24 16:37:02 +0100

    Fix webpack and index-prod.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -14,6 +14,7 @@ export default {
       exclude: /(node_modules|bower_components)/,
       use: [{
         loader: 'babel-loader',
+        options: {presets: ['@babel/preset-env']}
       }],
     }],
   },
