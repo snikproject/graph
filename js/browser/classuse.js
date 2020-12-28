2cc7b36b35d5865b5d59618abb9163fca8c7ef7d
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2020-12-28 16:45:08 +0100

    Class Use now exits Combine Matches if active. Resolve #341.

--- js/browser/classuse.js
+++ js/browser/classuse.js
@@ -10,140 +10,127 @@ Hides all other nodes. Resetting the view unhides the other nodes but keeps the 
 Recalculate the layout to place those nodes in relation to the whole graph again.
 @param {string} clazz The URI of the class.
 @param {string} subTop The sub top letter of the class (R,F or E)
- */
-export default function classUse(graph,clazz,subTop)
+*/
+export default async function classUse(graph,clazz,subTop)
 {
   graph.cy.startBatch();
-  let innerType = "meta:Role";
-  let middleType = "meta:Function";
-  let outerType = "meta:EntityType";
 
-  switch(subTop)
+  const types = {
+    "R":["meta:Role","meta:Function","meta:EntityType"],
+    "F":["meta:Function","meta:Role","meta:EntityType"],
+    "E":["meta:EntityType","meta:Function","meta:Role"]};
+
+  if(!(subTop in types))
   {
-    case "R":
-    {
-      innerType = "meta:Role";
-      middleType = "meta:Function";
-      outerType = "meta:EntityType";
-      break;
-    }
-    case "F":
-    {
-    // choose arbitrarily between the two possibilities and ask what the SNIK team thinks
-      innerType = "meta:Function";
-      middleType = "meta:Role";
-      outerType = "meta:EntityType";
-      break;
-    }
-    case "E":
-    {
-    // reverse of role
-      innerType = "meta:EntityType";
-      middleType = "meta:Function";
-      outerType = "meta:Role";
-      break;
-    }
-    default:
-    {
-      log.error("Unknown subtop. Cannot display class use.");
-      return;
-    }
+    log.error("Unknown subtop '"+subTop+"'. Cannot display class use.");
+    return;
   }
 
+  const [innerType,middleType,outerType] = types[subTop];
+
   const query =
-  `select distinct ?inner ?middle ?outer ?outerx
-  {
-    <${clazz}> (rdfs:subClassOf|skos:closeMatch|^skos:closeMatch)* ?inner.
-    ?inner meta:subTopClass ${innerType}.
-    OPTIONAL
+    `select distinct ?inner ?middle ?outer ?outerx
     {
-      ?inner ?p ?middle.
-      ?middle meta:subTopClass ${middleType}.`+
-//    ?role ?p ?f.
-//    ?f meta:subTopClass meta:Function.
-//    ?f (skos:closeMatch|^skos:closeMatch|^rdfs:subClassOf)* ?function.
-`
+      <${clazz}> (rdfs:subClassOf|skos:closeMatch|^skos:closeMatch)* ?inner.
+      ?inner meta:subTopClass ${innerType}.
       OPTIONAL
-      {`+
-//        #?function ?q ?et
-//        #?et meta:subTopClass meta:EntityType.
-`
-        ?middle ?q ?outer.
-        ?outer meta:subTopClass ${outerType}.
-        OPTIONAL {?outer (skos:closeMatch|^skos:closeMatch|^rdfs:subClassOf)+ ?outerx.}
-      }
-    }
-  }`;
-  sparql.select(query).then((json)=>
-  {
-    const inner = new Set();
-    const middle = new Set();
-    const outer = new Set();
-    const outerx = new Set();
-    for(let i=0;i<json.length;i++)
-    {
-      inner.add(json[i].inner.value);
-      if(json[i].middle) {middle.add(json[i].middle.value);}
-      if(json[i].outer) {outer.add(json[i].outer.value);}
-      if(json[i].outerx) {outerx.add(json[i].outerx.value);}
-    }
-    if(middle.size===0)
-    {
-      log.warn("Class "+clazz+" is not used.");
-      return;
-    }
-    // now we know we can display something
-    graph.resetStyle();
-    Graph.setVisible(graph.cy.elements(),false);
-    graph.starMode = true;
-
-    const classes = new Set([...inner, ...middle,...outer,...outerx]);
-    const selectedNodes = graph.cy.collection(`node[id='${clazz}']`);
-
-    for(const c of classes)
-    {
-      const cNodes = graph.cy.nodes(`node[id='${c}']`);
-      selectedNodes.merge(cNodes);
-      //selectedEdges = selectedEdges.union(cNodes.connectedEdges());
-    }
-
-    const selectedElements = selectedNodes.merge(selectedNodes.edgesWith(selectedNodes));
-
-    selectedNodes.layout(
       {
-        name: 'concentric',
-        fit: true,
-        levelWidth: function() {return 1;},
-        minNodeSpacing: 20,
-        concentric: function(node)
-        {
-          const uri = node.data(NODE.ID);
-          if(uri===clazz) {return 10;}
-          if(inner.has(uri)) {return 9;}
-          if(middle.has(uri)) {return 8;}
-          if(outer.has(uri)) {return 7;}
-          if(outerx.has(uri)) {return 6;}
-          return 10; // temporary workaround for inner without subtop
-          /*
-          // faster but can't discern expanded entity types from directly connected ones
-          switch(node.data(NODE.SUBTOP))
-          {
-          case "EntityType": return 1;
-          case "Function": return 2;
-          case "Role": return 3;
-          default: return 3; // temporary workaround for inner without subtop
+        ?inner ?p ?middle.
+        ?middle meta:subTopClass ${middleType}.`+
+        //    ?role ?p ?f.
+        //    ?f meta:subTopClass meta:Function.
+        //    ?f (skos:closeMatch|^skos:closeMatch|^rdfs:subClassOf)* ?function.
+        `
+        OPTIONAL
+        {`+
+          //        #?function ?q ?et
+          //        #?et meta:subTopClass meta:EntityType.
+          `
+          ?middle ?q ?outer.
+          ?outer meta:subTopClass ${outerType}.
+          OPTIONAL {?outer (skos:closeMatch|^skos:closeMatch|^rdfs:subClassOf)+ ?outerx.}
+        }
+      }
+    }`;
+
+  const json = await sparql.select(query);
+
+  const [inner, middle, outer, outerx]  = [...new Array(4)].map(()=>new Set());
+
+  for(let i=0;i<json.length;i++)
+  {
+    inner.add(json[i].inner.value);
+    if(json[i].middle) {middle.add(json[i].middle.value);}
+    if(json[i].outer) {outer.add(json[i].outer.value);}
+    if(json[i].outerx) {outerx.add(json[i].outerx.value);}
+  }
+  if(middle.size===0)
+  {
+    log.warn("Class "+clazz+" is not used.");
+    return;
+  }
+
+  // now we know we can display something
+
+  // class use does not work with combine matches enabled, disable it. See https://github.com/IMISE/snik-cytoscape.js/issues/341
+  const box = document.getElementById("combineMatchModeBox");
+  if(box.checked)
+  {
+    box.checked=false;
+    graph.combineMatch(false);
+    log.warn("Class Use is not compatible with Combine Matches. Disabling Combine Matches.");
+  }
+
+  graph.resetStyle();
+  Graph.setVisible(graph.cy.elements(),false);
+  graph.starMode = true;
+
+  const classes = new Set([...inner, ...middle,...outer,...outerx]);
+  const selectedNodes = graph.cy.collection(`node[id='${clazz}']`);
+
+  for(const c of classes)
+  {
+    const cNodes = graph.cy.nodes(`node[id='${c}']`);
+    selectedNodes.merge(cNodes);
+    //selectedEdges = selectedEdges.union(cNodes.connectedEdges());
+  }
+
+  const selectedElements = selectedNodes.merge(selectedNodes.edgesWith(selectedNodes));
+
+  selectedNodes.layout(
+    {
+      name: 'concentric',
+      fit: true,
+      levelWidth: function() {return 1;},
+      minNodeSpacing: 20,
+      concentric: function(node)
+      {
+        const uri = node.data(NODE.ID);
+        if(uri===clazz) {return 10;}
+        if(inner.has(uri)) {return 9;}
+        if(middle.has(uri)) {return 8;}
+        if(outer.has(uri)) {return 7;}
+        if(outerx.has(uri)) {return 6;}
+        return 10; // temporary workaround for inner without subtop
+        /*
+            // faster but can't discern expanded entity types from directly connected ones
+            switch(node.data(NODE.SUBTOP))
+            {
+            case "EntityType": return 1;
+            case "Function": return 2;
+            case "Role": return 3;
+            default: return 3; // temporary workaround for inner without subtop
           }
           */
-        },
       },
-    ).run();
+    },
+  ).run();
 
-    Graph.setVisible(selectedElements,true);
+  Graph.setVisible(selectedElements,true);
 
-    const centerNode = graph.cy.nodes(`node[id='${clazz}']`);
-    graph.cy.center(centerNode);
-    graph.cy.fit(selectedNodes);
-  },
-  );
+  const centerNode = graph.cy.nodes(`node[id='${clazz}']`);
+  graph.cy.center(centerNode);
+  graph.cy.fit(selectedNodes);
+
   graph.cy.endBatch();
 }
