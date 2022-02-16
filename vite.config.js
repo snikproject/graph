eb893249bf7411c6778d98d8dac0ae8158ee49eb
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-02-16 15:17:20 +0100

    Fix HTML documentation files.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const _dirname = typeof __dirname !== "undefined" ? __dirname : dirname(fileURLToPath(import.meta.url));
+
 export default {
 	sourcemap: true,
 	build: {
 		rollupOptions: {
+			input: {
+				main: resolve(_dirname, "index.html"),
+				contribute: resolve(_dirname, "html/contribute.html"),
+				layout: resolve(_dirname, "html/layoutHelp.html"),
+				manual: resolve(_dirname, "html/manual.html"),
+				troubleshooting: resolve(_dirname, "html/troubleshooting.html"),
+			},
 			output: {
 				assetFileNames: "assets/[name][extname]",
 			},
