c3984b16bc7a73a81e11bb107520bf632520cfa5
Hannes R. Brunsch <hrbrunsch+github@posteo.com>
2025-02-11 15:36:47 +0100

    refactor: remove unused utils

--- js/utils/string.ts
+++ js/utils/string.ts
@@ -4,17 +4,7 @@ import type { EdgeSingular } from "cytoscape";
 import * as rdf from "../rdf";
 import { EDGE } from "./constants";
 
-/**
- * Limit the input string to the maximum length. If it is longer, it will get cut and have two dots appended to exactly achieve the maximum length.
- * @param  s - potentially long input string to shorten
- * @param maxLength - maximum output string length
- * @returns the abbreviated input string */
-export function abbrv(s: string, maxLength: number = 25): string {
-	if (s.length < maxLength) {
-		return s;
-	}
-	return s.substring(0, maxLength - 2) + "..";
-} /** Creates a human readable string of the triple that an link represents.
+/** Creates a human readable string of the triple that an link represents.
  *  @param edge - the edge, whose label is determined
  *  @returns a human readable string of the triple that an edge represents. */
 
