165ff312abcfd87bcf138e131134bc8fbc3e2352
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2025-08-15 09:52:34 +0200

    fix: increase wheel sensitivity from 0.3 to 2 to fix extremely slow mouse scroll wheel zoom: resolve #461

--- js/browser/graph.ts
+++ js/browser/graph.ts
@@ -41,7 +41,7 @@ export class Graph {
 			container,
 			//@ts-expect-error concat type
 			style: style.style.concat(colorschemenight),
-			wheelSensitivity: 0.3,
+			wheelSensitivity: 2,
 			minZoom: 0.02,
 			maxZoom: 7,
 		});
